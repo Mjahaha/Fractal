4e9ce9a46f54c11f1f5c190c6afcd69eaf7d2fda
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-12-06 11:03:18 +1100

    got it mostly working as a POC but its twisted

---
+++ main.js
@@ -0,0 +1,88 @@
+let one = document.getElementById('one'); 
+let two = document.getElementById('two'); 
+let three = document.getElementById('three'); 
+
+let availableWidth = window.innerWidth - 100;
+let availableHeight = window.innerHeight - 100;
+
+function setPoints() {
+    if (availableWidth * 2 < availableHeight * 1.73) {
+        console.log('calculating by width');
+        let oneY = 0;
+        one.style.top = oneY + 'px';
+        let oneX = Math.floor(availableWidth / 2);
+        one.style.left = oneX + 'px' ;
+
+        let twoY = Math.floor(1.73 * availableWidth / 2);
+        two.style.top = twoY + 'px';
+        let twoX = 0;
+        two.style.left = twoX + 'px';
+
+        let threeY = Math.floor(1.73 * availableWidth / 2);
+        three.style.top = threeY + 'px';
+        let threeX = Math.floor(availableWidth);
+        three.style.left = threeX + 'px';
+        return [oneX, oneY, twoX, twoY, threeX, threeY];
+    } 
+    else {
+        console.log('calculating by height');
+        let oneY = 0
+        one.style.top = oneY + 'px';
+        let oneX = Math.floor(availableHeight / 1.73);
+        one.style.left = oneX + 'px';
+
+        let twoY = Math.floor(availableHeight);
+        two.style.top = twoY + 'px';
+        let twoX = 0
+        two.style.left = twoX + 'px';
+
+        let threeY = Math.floor(availableHeight);
+        three.style.top = threeY + 'px';
+        let threeX = Math.floor(availableHeight * 2 / 1.73);
+        three.style.left = threeX + 'px';
+        return [oneX, oneY, twoX, twoY, threeX, threeY];
+    }
+}
+
+let array = setPoints();
+let oneX = array[0];
+let oneY = array[1];
+let twoX = array[2];
+let twoY = array[3];
+let threeX = array[4];
+let threeY = array[5];
+
+let pointX = Math.floor(Math.random() * (threeX - twoX));
+let pointY = Math.floor(Math.random() * (twoY - oneY));
+
+console.log(pointX + ' ' + pointY);
+
+let randCorner;
+
+const addDot = () => {
+    let dot = document.createElement('div');
+    dot.classList.add('dot');
+    randCorner = Math.ceil(Math.random() * 3);
+    if (randCorner === 1) {
+        pointX = Math.abs((oneX + pointX) / 2);
+        pointY = Math.abs((oneY + pointY) / 2);
+        dot.style.top = pointX + 'px';
+        dot.style.left = pointY + 'px';
+    } else if (randCorner === 2) {
+        pointX = Math.abs((twoX + pointX) / 2);
+        pointY = Math.abs((twoY + pointY) / 2);
+        dot.style.top = pointX + 'px';
+        dot.style.left = pointY + 'px';
+    } else {
+        pointX = Math.abs((threeX + pointX) / 2);
+        pointY = Math.abs((threeY + pointY) / 2);
+        dot.style.top = pointX + 'px';
+        dot.style.left = pointY + 'px';
+    }
+    document.body.appendChild(dot);
+    console.log('added dot ' +pointX + ' ' + oneX);
+}
+
+for (let i = 0; i < 10000; i++) {
+    addDot();
+}
